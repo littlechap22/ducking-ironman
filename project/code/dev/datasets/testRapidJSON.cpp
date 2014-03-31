b014e1d4435efdd1327a9f419470682c67e3ec94
root <root@ubuntu.(none)>
2014-03-31 17:36:31 +0530

    Updating files used for rapidJson

--- project/code/dev/datasets/testRapidJSON.cpp
+++ project/code/dev/datasets/testRapidJSON.cpp
@@ -1,13 +1,36 @@
-#include "./headers/rapidjson/document.h"
+#include "./rapidjson/document.h"
 #include <cstdio>
+#include <fstream>
+#include <string>
+
+#include<algorithm>
+#include<cctype>
+#include<cstring>
+#include<functional>
+#include<iomanip>
+#include<iostream>
+#include<limits>
+#include<locale>
+#include<math.h>
+#include<sstream>
+#include<stdlib.h>
+#include<vector>
+
+using namespace std;
 
 int main() {
-	const char json[] = "{\"hello\" : \"world\" }";
+	//const char json[] = "{\"hello\" : \"world\" }";
+
+	std::ifstream ifs("road.json");
+	std:string content( (std::istreambuf_iterator<char>(ifs) ),
+				(std::istreambuf_iterator<char>() ));
+
 
 	rapidjson::Document d;
-	d.Parse<0>(json);
+	//d.Parse<0>(json);
+	d.Parse<0>(content.c_str());
 
-	printf("%s\n", d["hello"].GetString());
+	printf("%s\n", d["type"].GetString());
 
 	return 0;
 }
